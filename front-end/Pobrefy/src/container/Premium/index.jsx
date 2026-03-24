0099968ef02aaa31946fa7d8b0b47881a6fa4979
Xplosion123 <henrimelo06@gmail.com>
2026-03-23 22:56:44 -0300

    feat(premium): cria estrutura inicial da página premium

--- front-end/Pobrefy/src/container/Premium/index.jsx
+++ front-end/Pobrefy/src/container/Premium/index.jsx
@@ -1,5 +1,27 @@
-function Home() {
-  return <div>Home</div>
+import styles from "../../components/Header/Headerpremium.module.css";
+import { Link } from 'react-router-dom'
+import logo from '../../assets/spotifyfavicon.png'
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.left}>
+      <Link to="/">
+        <img src={logo} alt="pobrefy" className={styles.logo} />
+      </Link>
+      </div>
+      <nav className={styles.right}>
+        <ul>
+          <li><a href="#">Planos Premium</a></li> {/* Não é link, alterar como o do spotify original */}
+          <li><a href="#">Suporte</a></li>
+          <li><a href="#">Baixar</a></li>
+          <span className={styles.divider}>|</span>
+          <li><a href="#">Inscrever-se</a></li>
+          <li><a href="#">Entrar</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
-export default Home
+export default Header
